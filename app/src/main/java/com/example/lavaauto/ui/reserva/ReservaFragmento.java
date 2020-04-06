2e06be14e535350629f8efc4ce9bfe29fab14d77
augustob <bravoaugusto300@gmail.com>
2020-04-05 22:49:26 -0500

    Cambios en reserva

--- app/src/main/java/com/example/lavaauto/ui/reserva/ReservaFragmento.java
+++ app/src/main/java/com/example/lavaauto/ui/reserva/ReservaFragmento.java
@@ -4,17 +4,21 @@ import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.example.lavaauto.R;
 import com.example.lavaauto.ui.entidad.EServicio;
+import com.example.lavaauto.ui.servicio.DetalleServicioFragment;
 import com.example.lavaauto.ui.servicio.ServicioFragmento;
 
 import java.util.ArrayList;
@@ -27,6 +31,14 @@ public class ReservaFragmento extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_reserva, container, false);
+
+        Button btnSiguiente = (Button) view.findViewById(R.id.idBtnSiguiente1);
+        btnSiguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                eventoBotonSiguiente();
+            }
+        });
 
         listarDomicilio =new ArrayList<String>();
         listarDomicilio.add("AV. ZARUMILLA 856 - LA VICTORIA");
@@ -53,5 +65,13 @@ public class ReservaFragmento extends Fragment {
             textView1.setText(listarDomicilio.get(position));
             return item;
         }
+    }
+
+    private void eventoBotonSiguiente(){
+        Reserva2Fragment fgReserva2 = new Reserva2Fragment();
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction ft = fragmentManager.beginTransaction();
+        ft.replace(R.id.nav_host_fragment, fgReserva2);
+        ft.commit();
     }
 }
