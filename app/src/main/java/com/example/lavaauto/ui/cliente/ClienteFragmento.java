1277a7459c33249e9fb719777b3d57f782226ddb
joselopezcalixto <netzero60405@gmail.com>
2020-04-27 17:39:02 -0500

    Archivo pendiente - Detalle para opcion de Estado cambio de Fragment Cliente Agregacion de registro de clientes Cambios en la pantalla principal Cambio en Registrar usuarios Cambio en el login

--- app/src/main/java/com/example/lavaauto/ui/cliente/ClienteFragmento.java
+++ app/src/main/java/com/example/lavaauto/ui/cliente/ClienteFragmento.java
@@ -2,17 +2,16 @@ package com.example.lavaauto.ui.cliente;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 import com.example.lavaauto.R;
-import com.example.lavaauto.dao.LavaAutoDAO;
 import com.example.lavaauto.ui.entidad.EOrdenServicio;
 import com.example.lavaauto.ui.utilitario.Constants;
 
@@ -21,24 +20,36 @@ import java.util.ArrayList;
 public class ClienteFragmento extends Fragment {
     private ArrayList<EOrdenServicio> listarOrdenServicios;
     private int selectedPosition = 0;
-    public ClienteFragmento() {
-        // Required empty public constructor
-    }
+    private TextView txName, txdni, txcumpl, txreg, txorden;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        }
 
+        }
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_cliente, container, false);
-        listarOrdenServicios = new LavaAutoDAO().obtenerOrdenesServicioCliente(Constants.usuario.getUsuarioID());
-        ClienteFragmento.AdaptadorServicios adaptador = new ClienteFragmento.AdaptadorServicios(getActivity());
-        ListView lv1 = (ListView) view.findViewById(R.id.idlistordenusuario);
-        lv1.setAdapter(adaptador);
+//        listarOrdenServicios = new LavaAutoDAO().obtenerOrdenesServicioCliente(Constants.usuario.getUsuarioID());
+//        ClienteFragmento.AdaptadorServicios adaptador = new ClienteFragmento.AdaptadorServicios(getActivity());
+        //ListView lv1 = (ListView) view.findViewById(R.id.l);
+        //lv1.setAdapter(adaptador);
+        init(view);
         return view;
     }
+    private void init(View v){
+        txName = v.findViewById(R.id.txtname);
+        txName.setText(Constants.usuario.getNombre());
+        txdni = v.findViewById((R.id.txtdni));
+        txdni.setText(Constants.usuario.getDocume());
+        txcumpl=v.findViewById(R.id.txtfechanac);
+        txcumpl.setText(String.valueOf( Constants.usuario.getFecNac())) ;
+        Log.d("valor", String.valueOf(txcumpl));
+        txreg=v.findViewById(R.id.txtregistro);
+        txreg.setText(String.valueOf(Constants.usuario.getFecReg()));
+        Log.d("valor", String.valueOf(txreg));
+
+    }
+
     class AdaptadorServicios extends ArrayAdapter<EOrdenServicio> {
         AdaptadorServicios(Activity context) {
             super(context, R.layout.perfil_orden, listarOrdenServicios);
@@ -52,7 +63,6 @@ public class ClienteFragmento extends Fragment {
         TextView txtfecha = (TextView)item.findViewById(R.id.idTxtfecha);
         TextView txtestado = (TextView)item.findViewById(R.id.idTxtestado);
 
-      //  TextView name= txtNombreCliente;
 
         txtordenservicio.setText(String.valueOf(listarOrdenServicios.get(position).getOrdenID()));
         txtNombreCliente.setText(listarOrdenServicios.get(position).getUsuario().getNombre());
@@ -61,9 +71,9 @@ public class ClienteFragmento extends Fragment {
         return(item);
 
     }
-  //   public void regresar (View view){
-    //Intent retorno = new Intent(this, MenuNavegable.class);
-    //startActivity(retorno);
-    // }
+ //  public void regresar (View view){
+   // Intent retorno = new Intent(this, MenuNavegable.class);
+   // startActivity(retorno);
+   // }
 
 }
